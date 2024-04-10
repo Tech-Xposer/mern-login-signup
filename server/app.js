538d7ec6167746d8b7537f3d67260e2fd1834a2a
Ashutosh Sharma <asharma89505@gmail.com>
2024-04-10 11:31:19 +0530

    add welcome message

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const apiResponse = require("./handlers/response.handler");
 //configuring express
 const app = express();
 
@@ -18,6 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).json(apiResponse(200, "Welcome to AuthGuardian!"));
+});
 
 //routes
 app.use("/api/v1/user", require("./routes/user.routes"));
